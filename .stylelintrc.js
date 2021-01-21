374d6f4cd087c8f738c49261c5e6958e9337e849
Kenta Watashima <wtsmknt@gmail.com>
2021-01-21 14:35:17 +0900

    Fix stylelint configs

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,4 @@
 module.exports = {
-  extends: ['stylelint-config-recess-order', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  ignoreFiles: ['**/node_modules/**'],
 };
