98fc2f87c23091e7c81000d4a5b3c6a2b7ced748
Kenta Watashima <wtsmknt@gmail.com>
2021-02-09 17:21:27 +0900

    Fix stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   ignoreFiles: ['**/node_modules/**'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['function', 'if', 'each', 'include', 'mixin'],
+      },
+    ],
+  },
 };
